2437433125f02dc8549c3fd7e9a6a2d0849036ff
AndrewJohnsonmga <AndrewJohnsonmga@gmail.com>
2023-08-01 20:50:41 -0500

    Basic Canvas Complete

--- PencilKit2023/ViewController.swift
+++ PencilKit2023/ViewController.swift
@@ -5,15 +5,40 @@
 //  Created by Andrew Johnson on 8/1/23.
 //
 
+import PencilKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let canvasView: PKCanvasView = {
+        let canvas = PKCanvasView()
+        canvas.drawingPolicy = .anyInput
+        return canvas
+    }()
+    
+    private let toolPicker = PKToolPicker()
+    
+    let drawing = PKDrawing()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        canvasView.drawing = drawing
+        view.addSubview(canvasView)
     }
-
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        canvasView.frame = view.bounds
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        self.toolPicker.setVisible(true, forFirstResponder: canvasView)
+        self.toolPicker.addObserver(canvasView)
+        canvasView.becomeFirstResponder()
+    }
+    
+    
 }
-
